0dacdb4795d6dadb4766d3e50dfa51b74fe2de23
yarden_itzhaky <yardene015@gmail.com>
2024-10-18 23:24:54 +0300

    ffff

--- backend/index.js
+++ backend/index.js
@@ -104,6 +104,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', 'https://keeper-frontend-36zj.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
 
 app.use(passport.initialize());
 app.use(passport.session());
