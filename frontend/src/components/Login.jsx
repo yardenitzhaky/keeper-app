b0729fcb3fc185c432b1ed5713c3f968fdc43386
yarden_itzhaky <yardene015@gmail.com>
2024-10-18 17:01:16 +0300

    fdsa

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "./AuthContext";
 import axios from "axios";
 axios.defaults.withCredentials = true;
