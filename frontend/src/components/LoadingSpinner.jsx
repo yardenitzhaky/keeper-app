1ef03cb000c39e1524ca9a307713ad710adee5b3
yarden_itzhaky <yardene015@gmail.com>
2024-10-22 19:29:08 +0300

    fdsa

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -7,8 +7,7 @@ const LoadingSpinner = () => {
         <div className="spinner-ring outer"></div>
         <div className="spinner-ring inner"></div>
         <div className="spinner-dot"></div>
-        <div className="spinner-text">Loading...</div>
-        <div className="spinner-subtext">Please note: Initial load may take up to 50 seconds as the server spins up on Render's free tier.</div>
+        <div className="spinner-text">Loading... Initial load may take up to 50 seconds as the server spins up on Render's free tier. </div>
       </div>
     </div>
   );
