c564bab3b257e4b33b2a8333b5e2267420428666
yarden_itzhaky <yardene015@gmail.com>
2024-10-22 15:43:14 +0300

    use state fix

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import { useNavigate } from "react-router-dom";
 import HighlightIcon from "@mui/icons-material/Highlight";
