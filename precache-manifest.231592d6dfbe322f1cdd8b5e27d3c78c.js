c87f2aa93289deb7e85687cbe8fa203467f0241f
yarden_itzhaky <yardene015@gmail.com>
2025-02-25 14:54:36 +0200

    Updates

---
+++ precache-manifest.231592d6dfbe322f1cdd8b5e27d3c78c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90ae215587e612ba2bef893b0060ac8",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "29d72c4c85b91e4df89c",
+    "url": "/keeper-app/static/js/2.0ed541b6.chunk.js"
+  },
+  {
+    "revision": "8ec3b3e23633093868aa49236e1c8f5d",
+    "url": "/keeper-app/static/js/2.0ed541b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba33f916aaf273502b3",
+    "url": "/keeper-app/static/js/main.c43cf778.chunk.js"
+  },
+  {
+    "revision": "532ae620bc19f2850b7c",
+    "url": "/keeper-app/static/js/runtime-main.27d0de25.js"
+  }
+]);
